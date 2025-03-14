5a7bccf5673086e505005988f01db318f3df2dcf
Sofija <sofija.bozhinovska@damilah.com>
2025-03-14 09:13:10 +0100

    .

--- script2.py
+++ script2.py
@@ -1,8 +1,13 @@
 import pandas as pd
 from kestra import Kestra
+import sys
 
 # Read the parquet file into a DataFrame
-df = pd.read_parquet('SoldItemsPerYear.parquet')
+# Get the input file path from the command line arguments
+input_file_path = sys.argv[1]
+
+# Read the parquet file into a DataFrame
+df = pd.read_parquet(input_file_path)
 
 # Group by year and find the most sold product for each year
 most_sold_products = df.groupby('Year')['Item'].agg(lambda x: x.value_counts().idxmax())
